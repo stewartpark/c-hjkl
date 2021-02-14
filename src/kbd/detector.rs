4b80086ba26c849bdfffa2bf0220f2cdbe0e0771
Stewart Park <hello@stewartjpark.com>
2021-02-13 21:49:21 -0800

    Do not check keyboards too many times

--- src/kbd/detector.rs
+++ src/kbd/detector.rs
@@ -5,5 +5,6 @@ pub fn run_forever_until_keyboards_change() {
             if count == -1 { count = keyboards.len() as i8; }
             if count != keyboards.len() as i8 { break; }
         }
+        std::thread::sleep(std::time::Duration::from_secs(1));
     }
 }
